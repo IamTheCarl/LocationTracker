c3522e5cc2a4e8348caaf0143ae29334f6f653ff
I am the Carl <jamescarl96@gmail.com>
2020-07-26 22:29:22 -0400

    Added in some files that somehow got missed in the last commit.

--- build.rs
+++ build.rs
@@ -1,17 +1,24 @@
 use std::env;
-use std::fs::File;
-use std::io::Write;
 use std::path::PathBuf;
 
 fn main() {
-    // Put the linker script somewhere the linker can find it
-    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    File::create(out.join("memory.x"))
-        .unwrap()
-        .write_all(include_bytes!("memory.x"))
-        .unwrap();
-    println!("cargo:rustc-link-search={}", out.display());
+    let mut b = freertos_cargo_build::Builder::new();
+    let working = &PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
 
-    // Only re-run the build script when memory.x is changed.
+    // Path to FreeRTOS kernel or set ENV "FREERTOS_SRC" instead
+    b.freertos(format!("{}/FreeRTOS-Kernel", working.display()));
+    b.freertos_config("frtos_cfg");       // Location of `FreeRTOSConfig.h` 
+    b.freertos_port(String::from("GCC/ARM_CM4F")); // Port dir relative to 'FreeRTOS-Kernel/portable' 
+    b.heap::<String>(String::from("heap_4.c"));              // Set the heap_?.c allocator to use from 
+                                    // 'FreeRTOS-Kernel/portable/MemMang' (Default: heap_4.c)       
+
+    // b.get_cc().file("More.c");   // Optional additional C-Code to be compiled
+
+    b.compile().unwrap_or_else(|e| { panic!(e.to_string()) });
+
+    // Re-run the build script when config is changed.
+    println!("cargo:rerun-if-changed=frtos_cfg/FreeRTOSConfig.h");
+
+    // Re-run the build script when memory.x is changed.
     println!("cargo:rerun-if-changed=memory.x");
 }
