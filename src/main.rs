9715de42ce68cdb8609e75eb1170cda33ab2be04
I am the Carl <jamescarl96@gmail.com>
2020-07-26 20:18:50 -0400

    Installed FreeRTOS but it crashes when tasks try to yeild.

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,6 @@ use stm32f3xx_hal as hal;
 use hal::{
     gpio::GpioExt,
     rcc::RccExt,
-    pac::Interrupt,
 };
 
 use switch_hal::{
@@ -37,151 +36,66 @@ use switch_hal::{
     ToggleableOutputSwitch,
 };
 
-use rtic::cyccnt::{Instant, U32Ext as _};
-use cortex_m::peripheral::DWT;
+use freertos_rust::*; 
 
-#[rtic::app(device = stm32f3xx_hal::pac, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
-const APP: () = {
-    #[init(schedule = [blink_red, blink_green])]
-    fn init(mut cx: init::Context) {
-        // Setup logging.
-        let logger = Logger {
-            //Uses semihosting as destination with no interrupt control.
-            inner: Semihosting::<InterruptOk, _>::stdout()
-                .expect("Failed to get semihosting stdout"),
-            level: log::LevelFilter::Info
-        };
+#[allow(non_upper_case_globals)]
+#[no_mangle]
+pub static SystemCoreClock: u32 = 64_000_000;
 
-        // Tricks the compiler into thinking the logger is a static global.
-        // It dropping the logger after this will result in undefined behavior.
-        unsafe {
-            trick_init(&logger).ok();
-        }
+#[entry]
+fn main() -> ! {
+    // Setup logging.
+    let logger = Logger {
+        //Uses semihosting as destination with no interrupt control.
+        inner: Semihosting::<InterruptOk, _>::stdout()
+            .expect("Failed to get semihosting stdout"),
+        level: log::LevelFilter::Info
+    };
 
-        info!("Log setup.");
-
-        // Get handles to the device peripherals.
-        let dp = cx.device;
-
-        // Get the Reset and Clock Control. This gives us access to the AMBA High Preformance Bus, needed for the GPIOs.
-        let mut rcc = dp.RCC.constrain();
-
-        // Get the port.
-        let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
-
-        // Red LED is pin PE9.
-        let red = gpioe.pe9.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-        let mut red = red.into_active_high_switch();
-
-        // Green LED is pin PE15
-        let green = gpioe.pe15.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-        let mut green = green.into_active_low_switch();
-
-        red.on().ok();
-        green.on().ok();
-
-        // Initialize (enable) the monotonic timer (CYCCNT)
-        cx.core.DCB.enable_trace();
-        // required on Cortex-M7 devices that software lock the DWT (e.g. STM32F7)
-        DWT::unlock();
-        cx.core.DWT.enable_cycle_counter();
-
-        // semantically, the monotonic timer is frozen at time "zero" during `init`
-        // NOTE do *not* call `Instant::now` in this context; it will return a nonsense value
-        let now = cx.start; // the start time of the system
-
-        cx.schedule.blink_red(now + 2_000_000.cycles()).unwrap();
-        // cx.schedule.blink_green(now + 4_000_000.cycles()).unwrap();
+    // Tricks the compiler into thinking the logger is a static global.
+    // It dropping the logger after this will result in undefined behavior.
+    unsafe {
+        trick_init(&logger).ok();
     }
 
-    #[task]
-    fn blink_red(_c: blink_red::Context) {
-        info!("Blnk red!");
-    }
+    // Get handles to the core peripherals.
+    let _core = cortex_m::Peripherals::take().unwrap();
 
-    #[task]
-    fn blink_green(_c: blink_green::Context) {
-        info!("Blnk green!");
-    }
+    // Get handles to the device peripherals.
+    let dp = hal::pac::Peripherals::take().unwrap();
 
+    // Get the Reset and Clock Control. This gives us access to the AMBA High Preformance Bus, needed for the GPIOs.
+    let mut rcc = dp.RCC.constrain();
 
-    #[idle]
-    fn idle(_: idle::Context) -> ! {
+    // Get the port.
+    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+
+    // Red LED is pin PE9.
+    let red = gpioe.pe9.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut red = red.into_active_high_switch();
+
+    // Green LED is pin PE15
+    let green = gpioe.pe15.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    let mut green = green.into_active_low_switch();
+
+    red.on().ok();
+    green.on().ok();
+
+    Task::new().name("hello").stack_size(128).priority(TaskPriority(1)).start(move || {
+        info!("Task started.");
+
         loop {
-            // Will put the processor to sleep until the next interrupt happens.
-            // Saves us just a little power.
-            // asm::wfi();
-            asm::nop();
+            freertos_rust::CurrentTask::delay(Duration::ms(1000));
+            red.toggle().ok();
+            green.toggle().ok();
         }
-    }
+    }).unwrap();
 
-    // RTIC requires that unused interrupts are declared in an extern block when
-    // using software tasks; these free interrupts will be used to dispatch the
-    // software tasks.
-    extern "C" {
-        fn EXTI0();
-    }
-};
+    let control = cortex_m::register::control::read();
+    let privledge = control.npriv();
+    info!("Is privledge: {}", privledge.is_privileged());
 
-// #[entry]
-// fn main() -> ! {
-//     // Setup logging.
-//     let logger = Logger {
-//         //Uses semihosting as destination with no interrupt control.
-//         inner: Semihosting::<InterruptOk, _>::stdout()
-//             .expect("Failed to get semihosting stdout"),
-//         level: log::LevelFilter::Info
-//     };
-
-//     // Tricks the compiler into thinking the logger is a static global.
-//     // It dropping the logger after this will result in undefined behavior.
-//     unsafe {
-//         trick_init(&logger).ok();
-//     }
-
-
-//     // Get handles to the device peripherals.
-//     let dp = hal::pac::Peripherals::take().unwrap();
-
-//     // Get the Reset and Clock Control. This gives us access to the AMBA High Preformance Bus, needed for the GPIOs.
-//     let mut rcc = dp.RCC.constrain();
-
-//     // Get the port.
-//     let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
-
-//     // Red LED is pin PE9.
-//     let red = gpioe.pe9.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-//     let mut red = red.into_active_high_switch();
-
-//     // Green LED is pin PE15
-//     let green = gpioe.pe15.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-//     let mut green = green.into_active_low_switch();
-
-//     // let mut number = 0;
-
-//     red.on().ok();
-//     green.on().ok();
-
-//     loop {}
-
-//     // Task::new().name("hello").stack_size(128).priority(TaskPriority(1)).start(move || {
-//     //     info!("Task started.");
-
-//     //     loop {
-//     //         freertos_rust::CurrentTask::delay(Duration::ms(1000));
-//     //         red.toggle().ok();
-//     //         green.toggle().ok();
-//     //     }
-//     // }).unwrap();
-
-//     // FreeRtosUtils::start_scheduler();
-// }
-
-// We need a function to handle our allocation errors.
-#[alloc_error_handler]
-fn allocation_error_handler(_: core::alloc::Layout) -> ! {
-    // We just pass our error as a panic.
-    panic!("Allocation failure.");
+    FreeRtosUtils::start_scheduler();
 }
 
 #[exception]
@@ -213,4 +127,41 @@ fn DefaultHandler(irqn: i16) {
 #[exception]
 fn HardFault(ef: &ExceptionFrame) -> ! {
     panic!("{:#?}", ef);
+}
+
+#[exception]
+fn MemoryManagement() -> ! {
+    panic!("Memory management exception.");
+}
+
+#[exception]
+fn BusFault() -> !{
+    panic!("Bus fault.");
+}
+
+#[exception]
+fn UsageFault() -> ! {
+    panic!("Usage fault.");
+}
+
+#[global_allocator]
+static ALLOCATOR: FreeRtosAllocator = FreeRtosAllocator;
+
+// We need a function to handle our allocation errors.
+#[alloc_error_handler]
+fn allocation_error_handler(_: core::alloc::Layout) -> ! {
+    // We just pass our error as a panic.
+    panic!("Allocation failure.");
+}
+
+// We mark these as no_mangle so that the linker can link the FreeRTOS code to our handler functions.
+#[no_mangle]
+extern "C" fn vApplicationIdleHook() {
+    // Will put the processor to sleep until the next interrupt happens.
+    asm::wfi();
+}
+
+#[no_mangle]
+extern "C" fn vApplicationStackOverflowHook(_px_task: FreeRtosTaskHandle, pc_task_name: FreeRtosCharPtr) {
+    panic!("Stack overflow in task: {:#?}", pc_task_name);
 }
